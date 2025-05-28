5d6916a28d83c9bfaae00814574d6c7608dce18d
Nakalhouele <romeo.kone@outlook.com>
2025-05-28 10:34:15 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,6 @@
+# Ce script installe une version spécifique de python et de thonny sur une machine windows.
+# Il faut être vigiliant aux interactions avec l'ordinateur car des opérations manuelles pourraient être requises.
+
 # Définir la version de Python et les chemins d'installation
 $pythonVersion = "3.12.3"
 $pythonInstallerUrl = "https://www.python.org/ftp/python/$pythonVersion/python-$pythonVersion-amd64.exe"
@@ -20,3 +23,11 @@ Start-Process -FilePath $thonnyInstaller -ArgumentList "/VERYSILENT /DIR=$thonny
 Remove-Item $thonnyInstaller
 
 Write-Output "Installation terminée : Python $pythonVersion et Thonny ont été installés avec succès."
+
+
+
+# Après les installations (Manuellement)
+# 1. Mettre la langue en Francais au démarrage de thonny
+# 2. Changer la version de l'interpreteur Python dans thonny: 
+#     - Ouvrir thonny, cliquer sur Outils, sélectionner Options, sélectionner l'onglet Interpreteur, puis changer 
+#       l'executable pour faire pointer sur la version de python.
